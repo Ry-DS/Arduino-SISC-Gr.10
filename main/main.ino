2ae88d9f0f19c142bc9df85c8f7acf765dd0f6d3
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-06-30 18:12:32 +1000

    added button direction. Later implement to method

--- main/main.ino
+++ main/main.ino
@@ -2,6 +2,8 @@
 #define buzzer 7
 #define button 13
 int greenLed[]={3,5,9};
+int onLed=0;
+boolean right=false;
 void setup() {
   Serial.begin(9600);
   pinMode(redLed,OUTPUT);
@@ -17,9 +19,19 @@ void setup() {
 }
 
 void loop() {
+  delay(200);
   digitalWrite(redLed,HIGH);
   for(int i=0;i<sizeof(greenLed);i++){
-  digitalWrite(greenLed[i],HIGH);}
+    
+  digitalWrite(greenLed[i],LOW);}
+  digitalWrite(greenLed[onLed],HIGH);
+  if(digitalRead(button)==HIGH)
+  right=!right;
+  if(right)
+  onLed++;
+  else onLed--;
+  if(onLed>2)onLed=0;
+  if(onLed<0)onLed=2;
  
 
 }
