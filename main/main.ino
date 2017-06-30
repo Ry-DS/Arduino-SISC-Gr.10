289171c5f9746d3f190c39d06095649bf9f3931e
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-06-30 20:05:46 +1000

    Made siren and flashing lights work in conjunction

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,15 @@
 #define redLed 2
 #define buzzer 7
 #define button 13
+
 int greenLed[]={3,5,9};
 int sound[]={A0,A1};
-int onLed=0;
-int freq=0;
+boolean distressMode=true;
+
+
 boolean right=false;
+
+
 void setup() {
   Serial.begin(9600);
   pinMode(redLed,OUTPUT);
@@ -15,7 +19,9 @@ void setup() {
   pinMode(buzzer,OUTPUT);
   pinMode(button,INPUT);
   Serial.println("Done setting up Ports");
-  //tone(buzzer,1000);
+  tone(buzzer,4000);
+  delay(500);
+  noTone(buzzer);
   
   
   
@@ -23,15 +29,25 @@ void setup() {
 }
 
 void loop() {
-  delay(200);
+  delay(7);
   if(digitalRead(button)==HIGH)
   right=!right;
+  if(distressMode){
   flashLights(right);
+  //siren(); off cause ANNOYING
+  }
+  else{}//TODO make detection mode for distress. 
+  
  
 
 }
+
+int onLed=0;
+long timer=0;
 //parameter is direction. Call every loop
 void flashLights(boolean dir){
+  if(millis()-timer>=200){timer=millis();}
+  else return;
   int size=sizeof(greenLed);
   digitalWrite(redLed,HIGH);
   for(int i=0;i<size;i++){
@@ -48,3 +64,20 @@ void flashLights(boolean dir){
 
   
   }
+ boolean rise=true;
+ const int FREQ_TOP=5000;
+ const int FREQ_BOT=2000;
+ int freq=2000;
+//turn off with noTone(buzzer)
+void siren(){
+
+if(freq>FREQ_TOP)rise=false;
+else if(freq<FREQ_BOT)rise=true;
+if(rise) freq+=10;
+else freq-=10;
+tone(buzzer,freq);
+//Serial.println(freq);
+
+
+  
+}
