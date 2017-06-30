233f8c6d1e1e20aec939261b459669a1e3decf05
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-06-30 18:37:31 +1000

    Moved light flash to method

--- main/main.ino
+++ main/main.ino
@@ -2,16 +2,20 @@
 #define buzzer 7
 #define button 13
 int greenLed[]={3,5,9};
+int sound[]={A0,A1};
 int onLed=0;
+int freq=0;
 boolean right=false;
 void setup() {
   Serial.begin(9600);
   pinMode(redLed,OUTPUT);
+  pinMode(buzzer,OUTPUT);
   for(int i=0;i<sizeof(greenLed);i++){
   pinMode(greenLed[i],OUTPUT);}
   pinMode(buzzer,OUTPUT);
   pinMode(button,INPUT);
   Serial.println("Done setting up Ports");
+  //tone(buzzer,1000);
   
   
   
@@ -20,18 +24,27 @@ void setup() {
 
 void loop() {
   delay(200);
-  digitalWrite(redLed,HIGH);
-  for(int i=0;i<sizeof(greenLed);i++){
-    
-  digitalWrite(greenLed[i],LOW);}
-  digitalWrite(greenLed[onLed],HIGH);
   if(digitalRead(button)==HIGH)
   right=!right;
-  if(right)
-  onLed++;
-  else onLed--;
-  if(onLed>2)onLed=0;
-  if(onLed<0)onLed=2;
+  flashLights(right);
  
 
 }
+//parameter is direction. Call every loop
+void flashLights(boolean dir){
+  int size=sizeof(greenLed);
+  digitalWrite(redLed,HIGH);
+  for(int i=0;i<size;i++){
+    
+  digitalWrite(greenLed[i],LOW);}
+  digitalWrite(greenLed[onLed],HIGH);
+  
+  if(dir)
+  onLed++;
+  else onLed--;
+  size--;
+  if(onLed>2)onLed=0;
+  if(onLed<0)onLed=2;
+
+  
+  }
